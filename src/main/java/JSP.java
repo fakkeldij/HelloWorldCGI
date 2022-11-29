158eacc6ccae4ad6f6378b43f9f1a1f7eefd5796
Frank Fakkeldij <frank_fakkeldij@hotmail.com>
2022-11-29 10:32:01 +0100

    update remove junit from main class

--- src/main/java/JSP.java
+++ src/main/java/JSP.java
@@ -1,27 +1,4 @@
-	import java.util.concurrent.TimeUnit;
-
-	import org.junit.AfterClass; //Importing all the JUnit and Selenium classes
-	import org.junit.Assert;
-	import org.junit.BeforeClass;
-	import org.junit.Test;
-	import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-	import org.junit.AfterClass;
-
-
-	import org.junit.Assert;
-	import org.junit.BeforeClass;
-	import org.junit.Test;
-	import org.openqa.selenium.By;
-	import org.openqa.selenium.WebDriver;
-	import org.openqa.selenium.WebElement;
-	import org.openqa.selenium.firefox.FirefoxDriver;
-	import static org.junit.jupiter.api.Assertions.assertEquals;
-
-		
+	
 public class JSP {
 	
 }
