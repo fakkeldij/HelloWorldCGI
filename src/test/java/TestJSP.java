cd929432ad37cc16cef4a2e062dc8037e43b2599
Frank Fakkeldij <frank_fakkeldij@hotmail.com>
2022-11-29 16:24:48 +0100

    committing hello2

--- src/test/java/TestJSP.java
+++ src/test/java/TestJSP.java
@@ -1,6 +1,5 @@
 import org.junit.AfterClass;
 
-
 import org.junit.Assert;
 //import org.junit.BeforeClass;
 import org.junit.Test;
@@ -11,14 +10,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-
-
 import java.util.concurrent.TimeUnit;
 
 public class TestJSP {
-	
-	public static void main (String[] args) {
-		System.setProperty("webdriver.chrome.driver","/home/frankf/Downloads/drivers/chromedriver");
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "/home/frankf/Downloads/drivers/chromedriver");
 	}
 //	@Test
 //	public void test_JUnit() {
@@ -26,10 +23,9 @@ public class TestJSP {
 //	String str1="This is the testcase in this class";
 //	assertEquals("This is the testcase in this class", str1); 
 //	}
-	
 
 	@Test
-	public void TestWebsiteAvailability(){
+	public void TestWebsiteAvailability() {
 //		ChromeOptions options = new ChromeOptions();options.addArguments("--headless");
 //		WebDriver driver = new ChromeDriver(options); 
 //		driver.manage().window().maximize();
@@ -37,28 +33,22 @@ public class TestJSP {
 //				driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
 //				driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 //				driver.get("https://cgihelloworldwebapp.azurewebsites.net/HelloWorldCGI.jsp");
-				Assert.assertEquals("Hello World", "Hello World");
+		Assert.assertEquals("Hello World", "Hello World");
 //				Assert.assertEquals("Hello World", driver.getTitle());
 //				driver.quit();
-				
-		}
-		//WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
+
+	}
+	// WebDriver driver = new FirefoxDriver(); //Creating an object of FirefoxDriver
 //	      driver.get("https://trendoceans.com");
 //	      assertEquals("Hello World", driver.getTitle());
- 	  //Initialising the browser driver
+	// Initialising the browser driver
 ////	browser = webdriver.Chrome(executable_path="./drivers/chromedriver")
-   
 
-	//String str = "Hello World";
-     
-	
-
+	// String str = "Hello World";
 
 	@AfterClass
-	public static void closeBrowser(){
-	//driver.quit(); //Closing the driver once the tests are executed
+	public static void closeBrowser() {
+		// driver.quit(); //Closing the driver once the tests are executed
 	}
-	
 
-		
 }
